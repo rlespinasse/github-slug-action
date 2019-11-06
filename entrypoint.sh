93d0a6d6be790279ee41902ef23f16e18861027d
romain lespinasse <romain.lespinasse@gmail.com>
2019-11-06 09:49:03 +0100

    refactor: apply shellcheck recommandation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
 slug_ref() {
-    echo $1 \
-         | tr A-Z a-z \
-         | sed -r 's#refs/.*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g' \
+    echo "$1" \
+         | tr "[:upper:]" "[:lower:]" \
          | cut -c1-63
 }
 
-echo ::set-env name=GITHUB_REF_SLUG::$(slug_ref $GITHUB_REF)
-echo ::set-env name=GITHUB_HEAD_REF_SLUG::$(slug_ref $GITHUB_HEAD_REF)
-echo ::set-env name=GITHUB_BASE_REF_SLUG::$(slug_ref $GITHUB_BASE_REF)
+echo ::set-env name=GITHUB_REF_SLUG::"$(slug_ref "$GITHUB_REF")"
+echo ::set-env name=GITHUB_HEAD_REF_SLUG::"$(slug_ref "$GITHUB_HEAD_REF")"
+echo ::set-env name=GITHUB_BASE_REF_SLUG::"$(slug_ref "$GITHUB_BASE_REF")"
