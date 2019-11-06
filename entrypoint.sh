855ce255ddee3fb63d4e2b584fe52583a8692531
romain lespinasse <romain.lespinasse@gmail.com>
2019-11-06 01:27:30 +0100

    feat: add GITHUB_HEAD_REF_SLUG

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -l
 
-echo ::set-env name=GITHUB_REF_SLUG::$(echo $GITHUB_REF | tr A-Z a-z | sed -r 's#refs/.*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g' | cut -c1-63)
+slug_ref() {
+    echo $1 \
+         | tr A-Z a-z \
+         | sed -r 's#refs/.*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g' \
+         | cut -c1-63
+}
+
+echo ::set-env name=GITHUB_REF_SLUG::$(slug_ref $GITHUB_REF)
+echo ::set-env name=GITHUB_HEAD_REF_SLUG::$(slug_ref $GITHUB_HEAD_REF)
