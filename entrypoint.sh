78b4cbe021c79c19f85c7fb1d395c89cd85e1aeb
rlespinasse <romain.lespinasse@gmail.com>
2020-04-25 22:03:46 +0200

    style: add editorconfig file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 
 slug_ref() {
-    echo "$1" \
-         | tr "[:upper:]" "[:lower:]" \
-         | sed -r 's#refs/[^\/]*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g;s/^-*//;s/-*$//' \
-         | cut -c1-63
+  echo "$1" |
+    tr "[:upper:]" "[:lower:]" |
+    sed -r 's#refs/[^\/]*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g;s/^-*//;s/-*$//' |
+    cut -c1-63
 }
 
-short_sha(){
-  echo "$1" \
-        | cut -c1-8
+short_sha() {
+  echo "$1" |
+    cut -c1-8
 }
 
 echo ::set-env name=GITHUB_REF_SLUG::"$(slug_ref "$GITHUB_REF")"
