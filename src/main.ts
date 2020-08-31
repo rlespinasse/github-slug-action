6d4bc7429eb8f5008dc87208e02cf20dbea0e5cd
Antoine Meausoone <antoine.meausoone@gmail.com>
2020-08-31 21:23:43 +0200

    feat: implement event slug variables

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import * as core from '@actions/core'
 import {slugref, slugurl, slugurlref, shortsha} from './slug'
 
-//TODO
-// echo ::set-env name=GITHUB_EVENT_REF_SLUG::"$(slug_ref "$(get_event_keyvalue "ref")")"
-// echo ::set-env name=GITHUB_EVENT_REF_SLUG_URL::"$(slug_url_ref "$(get_event_keyvalue "ref")")"
 async function run(): Promise<void> {
   try {
+    let eventPath = process.env['GITHUB_EVENT_PATH']
+    if (eventPath != null) {
+      const eventData = await import(eventPath!)
+      
+      core.exportVariable('GITHUB_EVENT_REF_SLUG', slugref(eventData.ref))
+      core.exportVariable('GITHUB_EVENT_REF_SLUG_URL', slugurlref(eventData.ref))
+    }
+
     exportSlugRef('GITHUB_REPOSITORY_SLUG', 'GITHUB_REPOSITORY')
     exportSlug('GITHUB_REPOSITORY_SLUG_URL', 'GITHUB_REPOSITORY')
 
     exportSlugRef('GITHUB_REF_SLUG', 'GITHUB_REF')
     exportSlugRef('GITHUB_HEAD_REF_SLUG', 'GITHUB_HEAD_REF')
     exportSlugRef('GITHUB_BASE_REF_SLUG', 'GITHUB_BASE_REF')
-    // echo ::set-env name=GITHUB_EVENT_REF_SLUG::"$(slug_ref "$(get_event_keyvalue "ref")")"
 
     exportSlugUrlRef('GITHUB_REF_SLUG_URL', 'GITHUB_REF')
     exportSlugUrlRef('GITHUB_HEAD_REF_SLUG_URL', 'GITHUB_HEAD_REF')
     exportSlugUrlRef('GITHUB_BASE_REF_SLUG_URL', 'GITHUB_BASE_REF')
-    // exportSlugUrlRef('GITHUB_EVENT_REF_SLUG_URL','')
-    // echo ::set-env name=GITHUB_EVENT_REF_SLUG_URL::"$(slug_url_ref "$(get_event_keyvalue "ref")")"
 
     exportShortSha('GITHUB_SHA_SHORT', 'GITHUB_SHA')
   } catch (error) {
