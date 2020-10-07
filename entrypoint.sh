d678898ce55139d0005c35618747506fbeb2d1ed
rlespinasse <romain.lespinasse@gmail.com>
2020-10-07 22:22:13 +0200

    fix: remove set-env due to CVE fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,17 +39,16 @@ get_event_keyvalue() {
   fi
 }
 
-echo ::set-env name=GITHUB_REPOSITORY_SLUG::"$(slug "$GITHUB_REPOSITORY")"
-echo ::set-env name=GITHUB_REPOSITORY_SLUG_URL::"$(slug_url "$GITHUB_REPOSITORY")"
-
-echo ::set-env name=GITHUB_REF_SLUG::"$(slug_ref "$GITHUB_REF")"
-echo ::set-env name=GITHUB_HEAD_REF_SLUG::"$(slug_ref "$GITHUB_HEAD_REF")"
-echo ::set-env name=GITHUB_BASE_REF_SLUG::"$(slug_ref "$GITHUB_BASE_REF")"
-echo ::set-env name=GITHUB_EVENT_REF_SLUG::"$(slug_ref "$(get_event_keyvalue "ref")")"
-
-echo ::set-env name=GITHUB_REF_SLUG_URL::"$(slug_url_ref "$GITHUB_REF")"
-echo ::set-env name=GITHUB_HEAD_REF_SLUG_URL::"$(slug_url_ref "$GITHUB_HEAD_REF")"
-echo ::set-env name=GITHUB_BASE_REF_SLUG_URL::"$(slug_url_ref "$GITHUB_BASE_REF")"
-echo ::set-env name=GITHUB_EVENT_REF_SLUG_URL::"$(slug_url_ref "$(get_event_keyvalue "ref")")"
-
-echo ::set-env name=GITHUB_SHA_SHORT::"$(short_sha "$GITHUB_SHA")"
+{
+  echo "GITHUB_REPOSITORY_SLUG=$(slug "$GITHUB_REPOSITORY")"
+  echo "GITHUB_REPOSITORY_SLUG_URL=$(slug_url "$GITHUB_REPOSITORY")"
+  echo "GITHUB_REF_SLUG=$(slug_ref "$GITHUB_REF")"
+  echo "GITHUB_HEAD_REF_SLUG=$(slug_ref "$GITHUB_HEAD_REF")"
+  echo "GITHUB_BASE_REF_SLUG=$(slug_ref "$GITHUB_BASE_REF")"
+  echo "GITHUB_EVENT_REF_SLUG=$(slug_ref "$(get_event_keyvalue "ref")")"
+  echo "GITHUB_REF_SLUG_URL=$(slug_url_ref "$GITHUB_REF")"
+  echo "GITHUB_HEAD_REF_SLUG_URL=$(slug_url_ref "$GITHUB_HEAD_REF")"
+  echo "GITHUB_BASE_REF_SLUG_URL=$(slug_url_ref "$GITHUB_BASE_REF")"
+  echo "GITHUB_EVENT_REF_SLUG_URL=$(slug_url_ref "$(get_event_keyvalue "ref")")"
+  echo "GITHUB_SHA_SHORT=$(short_sha "$GITHUB_SHA")"
+} >>"$GITHUB_ENV"
