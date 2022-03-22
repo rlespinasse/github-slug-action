3a7cbf44fdde028240cb137cc202f6b6277d5fea
rlespinasse <romain.lespinasse@gmail.com>
2022-03-22 10:01:36 +0100

    fix: set short-length automatically when no checkout

--- preflight.sh
+++ preflight.sh
@@ -12,3 +12,23 @@ if [ ! "${INPUT_SHORT_LENGTH}" -eq "${INPUT_SHORT_LENGTH}" ] 2>/dev/null; then
   echo "::error ::short-length must be a number"
   exit 1
 fi
+
+echo "::debug ::Control preflight short-length setup"
+PREFLIGHT_SHORT_LENGTH=""
+if [ -n "${INPUT_SHORT_LENGTH}" ]; then
+  PREFLIGHT_SHORT_LENGTH="${INPUT_SHORT_LENGTH}"
+  echo "::debug ::Use input value: $PREFLIGHT_SHORT_LENGTH"
+elif [ "$(git rev-parse --is-inside-work-tree)" == "true" ]; then
+  echo "::debug ::The action run inside a git repository, short-length can be empty"
+else
+  echo "::debug ::The action run outside a git repository, need to set short-length"
+  PREFLIGHT_SHORT_LENGTH="$(git config --get core.abbrev)"
+  if [ -n "${PREFLIGHT_SHORT_LENGTH}" ]; then
+    echo "::debug ::Git Configuration 'core.abbrev' is set, using it as short-length"
+  else
+    PREFLIGHT_SHORT_LENGTH="7"
+    echo "::debug ::Using fallback value for short-length"
+  fi
+fi
+echo "::debug ::Set PREFLIGHT_SHORT_LENGTH=$PREFLIGHT_SHORT_LENGTH"
+echo "::set-output name=PREFLIGHT_SHORT_LENGTH::$PREFLIGHT_SHORT_LENGTH"
